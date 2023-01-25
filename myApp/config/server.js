50467104cb8e4add06d9a942d32298504789b423
Poteau Florian <florianpoteau59@gmail.com>
2023-01-25 17:46:18 +0100

    contact changement server.js et install cors

--- myApp/config/server.js
+++ myApp/config/server.js
@@ -1,7 +1,19 @@
+// const corsOptions = {
+//   origin: 'http://127.0.0.1:5500',
+//   methods: 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+//   headers: 'Content-Type, Authorization',
+//   credentials: true
+// };
+
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
     keys: env.array('APP_KEYS'),
+    // middlewares: {
+    //   before: [
+    //     cors(corsOptions)
+    //   ],
+    // },
   },
 });
