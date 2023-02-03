5712317e6519325c629e1eba8ca256cdca904b7e
mehdijouihri <100852016+mehdijouihri@users.noreply.github.com>
2023-02-03 10:27:50 +0100

    add strapi pour quisommesnous

--- java.js
+++ java.js
@@ -1,51 +1,138 @@
 
-//document.querySelector
 
-// texte de l'actualité 1
-const image1Div = document.querySelector("link-title");
+let mybutton = document.getElementById("myBtn");
 
-// Date de l'actualité 1
-const image2Div = document.getElementById("dateactu1");
 
-// Image de l'actualité 1
-const imageStrapi1 = document.querySelector(".home-img");
+window.onscroll = function () { scrollFunction() };
 
-// Date de l'actualité 2
-const image3Div = document.getElementById("dateactu2");
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
 
-// texte de l'actualité 2
-const image4Div = document.getElementById("text2");
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
 
-// Image de l'actualité 2
-const imageStrapi2 = document.querySelector(".imageactu2");
 
-// Date de l'actualité 3
+// document.querySelector
 
-const image5Div = document.getElementById("dateactu3");
+// Nos services en un clic
 
-// texte de l'actualité 3
+// image1
+const image1Div = document.querySelector(".home-img");
 
-const image6Div = document.getElementById("text3");
+// texte1
+const texte1Div = document.querySelector(".link-title");
 
-// image de l'actualité 3
+// Image2
+const image2Div = document.querySelector(".home-img2");
 
-const imageStrapi3 = document.querySelector(".imageactu3");
+// texte2
+const texte2Div = document.querySelector(".link-title2");
 
-// Date de l'actualité 4
+// image3
+const image3Div = document.querySelector(".home-img3");
 
-const image7Div = document.getElementById("dateactu4");
+// texte3
+const texte3Div = document.querySelector(".link-title3");
 
-// texte de l'actualité 4
 
-const image8Div = document.getElementById("text4");
+// Nos actualités
 
-// image de l'actualité 4
+// image4
+const image4Div = document.querySelector(".img4");
 
-const imageStrapi4 = document.querySelector(".imageactu4");
+// Titre4
+const titre4Div = document.getElementById("activite");
+
+// Texte4
+const texte4Div = document.querySelector(".event-text");
+
+
+// image5
+const image5Div = document.querySelector(".img5");
+
+// titre5
+const titre5Div = document.getElementById("festival");
+
+// texte5
+const texte5Div = document.querySelector(".event-text1");
+
+
+// image6
+const image6Div = document.querySelector(".img6");
+
+// titre6
+const titre6Div = document.getElementById("parade");
+
+// texte6
+const texte6Div = document.querySelector(".event-text2");
+
+
+// #perche tendue
+
+// image7
+
+const image7Div = document.querySelector(".img7");
+
+// Titre7
+
+const titre7Div = document.getElementById("nous1");
+
+// Texte7
+
+const texte7Div = document.getElementById("texteimg7");
+
+// date7
+
+const date7Div = document.getElementById("date1");
+
+// image8
+
+const image8Div = document.querySelector(".img8");
+
+// Titre8
+
+const titre8Div = document.getElementById("nous2");
+
+// Texte8
+
+const texte8Div = document.getElementById("texteimg8");
+
+// date8
+
+const date8Div = document.getElementById("date2");
+
+// image9
+
+const image9Div = document.querySelector(".img9");
+
+// Titre9
+
+const titre9Div = document.getElementById("nous3");
+
+// Texte9
+
+const texte9Div = document.getElementById("texteimg9");
+
+// date9
+
+const date9Div = document.getElementById("date3");
+
+
+
+
+
 
 
 const url = "http://localhost:1337";
-const image2 = "http://localhost:1337/api/posts?populate=*";
+const image2 = "http://localhost:1337/api/aceuils?populate=*";
 
 
 let allactu = [];
@@ -59,27 +146,89 @@ function init() {
 // Fonction uniquement pour les élément de type texte dans les actualités
 
 function gettexte() {
-    fetch(url + "/api/posts")
+    fetch(url + "/api/aceuils")
         .then((data) => data.json())
         .then((result) => {
             allactu = result;
             console.log("Allactu", allactu);
 
-            // actualité 1
+            // texte "Nos service en un clic"
 
-            image1Div.innerHTML = `${JSON.stringify(allactu.data[0].attributes.texte)}`;
+            // // Texte 1
 
-            // actualité 2
+            texte1Div.innerHTML = `${JSON.stringify(allactu.data[0].attributes.titre)}`;
 
-            image4Div.innerHTML = `${JSON.stringify(allactu.data[1].attributes.texte)}`;
+            // Texte 2
 
-            // actualité 3
+            texte2Div.innerHTML = `${JSON.stringify(allactu.data[1].attributes.titre)}`;
 
-            image6Div.innerHTML = `${JSON.stringify(allactu.data[2].attributes.texte)}`;
+            // Texte 3
 
-            // actualité 4
+            texte3Div.innerHTML = `${JSON.stringify(allactu.data[2].attributes.titre)}`;
 
-            image8Div.innerHTML = `${JSON.stringify(allactu.data[3].attributes.texte)}`;
+            // Nos actualités
+
+            // titre 1
+
+            titre4Div.innerHTML = `${JSON.stringify(allactu.data[3].attributes.titre)}`;
+
+            // texte 1
+
+            texte4Div.innerHTML = `${JSON.stringify(allactu.data[3].attributes.texte)}`;
+
+            // titre 2
+
+            titre5Div.innerHTML = `${JSON.stringify(allactu.data[4].attributes.titre)}`;
+
+            // texte 2
+
+            texte5Div.innerHTML = `${JSON.stringify(allactu.data[4].attributes.texte)}`;
+
+            // titre 3
+
+            titre6Div.innerHTML = `${JSON.stringify(allactu.data[5].attributes.titre)}`;
+
+            // texte 3
+
+            texte6Div.innerHTML = `${JSON.stringify(allactu.data[5].attributes.texte)}`;
+
+            // # Perche tendue
+
+            // Titre1
+
+            titre7Div.innerHTML = `${JSON.stringify(allactu.data[6].attributes.titre)}`;
+
+            // Texte1
+
+            texte7Div.innerHTML = `${JSON.stringify(allactu.data[6].attributes.texte)}`;
+
+            // date 1
+
+            date7Div.innerHTML = `${JSON.stringify(allactu.data[6].attributes.date)}`;
+
+            // Titre 2
+
+            titre8Div.innerHTML = `${JSON.stringify(allactu.data[7].attributes.titre)}`;
+
+            // Texte 2
+
+            texte8Div.innerHTML = `${JSON.stringify(allactu.data[7].attributes.texte)}`
+
+            // date 2
+
+            date8Div.innerHTML = `${JSON.stringify(allactu.data[7].attributes.date)}`;
+
+            // Titre3
+
+            titre9Div.innerHTML = `${JSON.stringify(allactu.data[8].attributes.titre)}`;
+
+            // Texte3
+
+            texte9Div.innerHTML = `${JSON.stringify(allactu.data[8].attributes.texte)}`;
+
+            // date 3
+
+            date9Div.innerHTML = `${JSON.stringify(allactu.data[8].attributes.date)}`;
 
         })
         .catch((err) => {
@@ -91,30 +240,50 @@ function gettexte() {
 // Fonction uniquement pour les images des actualités
 
 function getimage() {
-    fetch(url + "/api/posts?populate=*")
+    fetch(url + "/api/aceuils?populate=*")
 
         .then((data) => data.json())
         .then((result) => {
             allactu = result;
             console.log("Allactu", allactu);
 
-            // Actualité 1
+            // image 1
 
-            imageStrapi1.src = "http://localhost:1337" + `${allactu.data[0].attributes.image.data[0].attributes.formats.thumbnail.url}`;
-            console.log(imageStrapi1.src);
+            image1Div.src = url + `${allactu.data[0].attributes.image.data[0].attributes.formats.medium.url}`;
+            console.log(image1Div.src);
             console.log("AllActu", allactu.data);
 
-            // Actualité 2
+            // image 2
 
-            imageStrapi2.src = url + `${allactu.data[1].attributes.image.data[0].attributes.formats.thumbnail.url}`;
+            image2Div.src = url + `${allactu.data[1].attributes.image.data[0].attributes.formats.medium.url}`;
 
-            // Actualité 3
+            // image 3
 
-            imageStrapi3.src = url + `${allactu.data[2].attributes.image.data[0].attributes.formats.thumbnail.url}`;
+            image3Div.src = url + `${allactu.data[2].attributes.image.data[0].attributes.formats.medium.url}`;
 
-            // Actualité 4
+            // image 4
 
-            imageStrapi4.src = url + `${allactu.data[3].attributes.image.data[0].attributes.formats.thumbnail.url}`;
+            image4Div.src = url + `${allactu.data[3].attributes.image.data[0].attributes.formats.thumbnail.url}`;
+
+            // image 5
+
+            image5Div.src = url + `${allactu.data[4].attributes.image.data[0].attributes.formats.thumbnail.url}`;
+
+            // image 6
+
+            image6Div.src = url + `${allactu.data[5].attributes.image.data[0].attributes.formats.thumbnail.url}`;
+
+            // image7
+
+            image7Div.src = url + `${allactu.data[6].attributes.image.data[0].attributes.formats.thumbnail.url}`;
+
+            // image 8
+
+            image8Div.src = url + `${allactu.data[7].attributes.image.data[0].attributes.formats.thumbnail.url}`;
+
+            // image 9
+
+            image9Div.src = url + `${allactu.data[8].attributes.image.data[0].attributes.formats.thumbnail.url}`;
 
         })
         .catch((err) => {
