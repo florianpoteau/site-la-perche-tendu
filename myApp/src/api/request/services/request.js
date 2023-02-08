7f73165a514b63abe7545b885cd1fd068fba526b
Poteau Florian <florianpoteau59@gmail.com>
2023-02-08 11:44:41 +0100

    push

--- myApp/src/api/request/services/request.js
+++ myApp/src/api/request/services/request.js
@@ -30,17 +30,7 @@ send: async (ctx) => {
 
 
 
-// await strapi.plugins['email'].services.email.send({
-//     to: formData.email,
-//     from: 'florianpoteau59@outlook.fr', //e.g. single sender verification in SendGrid
-//     cc: 'florianpoteau59@outlook.fr',
-//     bcc: 'florianpoteau59@outlook.fr',
-//     replyTo: 'florianpoteau59@outlook.fr',
-//     subject: 'The Strapi Email plugin worked successfully',
-//     text: 'Hello world!',
-//     html: 'Hello world!',
 
-// });
 
 
 
